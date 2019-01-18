bd1424c2f8cc2e9f80da824ecf9149a63fb59169
BIT <BIT@DESKTOP-E23PFRN>
2019-01-18 09:44:15 +0900

    190118 09:44 jdbc start

---
+++ src/com/douzon/jdbc/test/SelectTest.java
@@ -0,0 +1,42 @@
+package com.douzon.jdbc.test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class SelectTest {
+
+	public static void main(String[] args) {
+		Connection conn = null;
+		try {
+			// 1. JDBC Driver(MYSQL) 로딩
+			Class.forName("com.mysql.jdbc.Driver");
+			//pripertiy -> build path를 등록해줘야된다.
+
+			// 2. 연결하기 ( jdbc:연결할database://ip:port/database이름 ) port번호는 생략가능하다.
+			// url과 id와 password를 같이 입력해준다. (Connection 객체 얻어오기)
+			String url = "jdbc:mysql://localhost:3306/webdb";
+			conn = DriverManager.getConnection(url,"webdb","webdb");
+			System.out.println("연결 성공");
+
+			// 3. Statement 객체를 생성
+			Statement stmt = conn.createStatement();
+
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로딩 실패:"+e);
+		} catch (SQLException e) {
+			System.out.println("error:"+e);
+		} finally {
+			try {
+				if(conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
